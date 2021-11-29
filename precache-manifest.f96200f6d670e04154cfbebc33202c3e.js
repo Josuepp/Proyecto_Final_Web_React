9d5de66e847152425932659f6d99f8a387124264
Josuepp <62664664+Josuepp@users.noreply.github.com>
2021-11-28 21:04:41 -0600

    Updates

---
+++ precache-manifest.f96200f6d670e04154cfbebc33202c3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c48e18a75014f13a327cc8a5f79392",
+    "url": "/Proyecto_Final_Web_React/index.html"
+  },
+  {
+    "revision": "d00ba0f6b17bb7120f23",
+    "url": "/Proyecto_Final_Web_React/static/css/main.a507bbd2.chunk.css"
+  },
+  {
+    "revision": "3a64eb4e75232d27fbbc",
+    "url": "/Proyecto_Final_Web_React/static/js/2.be736085.chunk.js"
+  },
+  {
+    "revision": "0569f079e111a6d4f0bf8418a634cb07",
+    "url": "/Proyecto_Final_Web_React/static/js/2.be736085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e23b798db19b14ba16",
+    "url": "/Proyecto_Final_Web_React/static/js/3.76126ac3.chunk.js"
+  },
+  {
+    "revision": "d00ba0f6b17bb7120f23",
+    "url": "/Proyecto_Final_Web_React/static/js/main.abbb88fb.chunk.js"
+  },
+  {
+    "revision": "1fcfce9b5308b52f4d35",
+    "url": "/Proyecto_Final_Web_React/static/js/runtime-main.dc6fa43e.js"
+  },
+  {
+    "revision": "49c53331d19be74b52d47fcce7e97468",
+    "url": "/Proyecto_Final_Web_React/static/media/programmer.49c53331.png"
+  }
+]);
